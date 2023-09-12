9e6d1897f64f602aea07b1d76e47782ccc14cab1
Orion Rosales <osrosalesalfaro@crimson.ua.edu>
2023-09-12 09:38:07 -0500

    First commit

---
+++ FL2023-Assignment1-orosalesalfaroalfaro/Program.cs
@@ -0,0 +1,15 @@
+namespace FL2023_Assignment1_orosalesalfaroalfaro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
